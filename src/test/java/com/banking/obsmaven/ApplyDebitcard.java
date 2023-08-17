e5f2d68603b85b9961bfbe0915a7c73f46f4d3cc
91784 <91784@amlan>
2023-08-17 11:13:44 +0530

    debit card applied

---
+++ src/test/java/com/banking/obsmaven/ApplyDebitcard.java
@@ -0,0 +1,15 @@
+package com.banking.obsmaven;
+
+import org.testng.annotations.Test;
+
+public class ApplyDebitcard {
+	
+	
+	@Test
+	public void applyDebitcard() {
+		
+		
+		System.out.println("---debitcard applied---");
+	}
+
+}

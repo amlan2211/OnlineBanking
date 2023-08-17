0121af557d3c60459ffc5fb348ec3ecbd515850c
91784 <91784@amlan>
2023-08-17 11:02:46 +0530

    Deleted customer updated

---
+++ src/test/java/com/banking/obsmaven/DeleteCust.java
@@ -0,0 +1,18 @@
+package com.banking.obsmaven;
+
+import org.testng.annotations.Test;
+
+public class DeleteCust 
+{
+@Test 
+public void deletecust()
+{
+	System.out.println("customer is deleted");
+	
+	}
+@Test
+public void custmerDel()
+{
+	System.out.println("customer is deleted");
+	}
+}

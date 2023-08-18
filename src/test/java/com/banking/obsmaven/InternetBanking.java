35e662eafacfc438e6bb862a9a9db6fb967d9d01
amlan2211 <amlankmishra@gmail.com>
2023-08-18 14:32:31 +0530

    Internet banking

---
+++ src/test/java/com/banking/obsmaven/InternetBanking.java
@@ -0,0 +1,13 @@
+package com.banking.obsmaven;
+
+import org.testng.annotations.Test;
+
+public class InternetBanking 
+{
+@Test
+public void internetbanking()
+{
+	System.out.println("internet banking ");
+	}
+	
+}

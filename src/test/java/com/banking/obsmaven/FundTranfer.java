0427c2c05c153a53773a9cb23ef7bf5a77893a27
amlan2211 <amlankmishra@gmail.com>
2023-08-18 17:30:23 +0530

    Smoke

---
+++ src/test/java/com/banking/obsmaven/FundTranfer.java
@@ -0,0 +1,13 @@
+package com.banking.obsmaven;
+
+import org.testng.annotations.Test;
+
+public class FundTranfer 
+{
+	@Test
+	public void Fund()
+	{
+		System.out.println("fund tranfered");
+	}
+
+}
